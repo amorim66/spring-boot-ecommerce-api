1d4c2323f0abb12b96f3ab7e8349953f0433fc81
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-03-25 14:22:16 -0300

    teste ci/cd

--- src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
-
+    //Service checkout
     private CheckoutService checkoutService;
 
     @Autowired

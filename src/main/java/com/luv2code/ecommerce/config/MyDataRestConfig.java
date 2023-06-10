df882bfbfd2b41163d62986ad4d80e97b6e6c9ad
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-06-10 14:24:08 -0300

    cover customer repository from api

--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -34,7 +34,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         //disable HTTP methods for Product: PÚT, POST, and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);

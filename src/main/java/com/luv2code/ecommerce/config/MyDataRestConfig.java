74de63253d3acd57f531a69e4f2c727ccc838b2e
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-06-11 02:38:34 -0300

    develop configs

--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -46,7 +46,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         exposeIds(config);
 
         // configure cors mapping
-        cors.addMapping( "/**").allowedOrigins(theAllowedOrigins);
+        cors.addMapping( config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
